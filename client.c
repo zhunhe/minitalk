cd83edc61dd782455060d9e1a6514497eb494955
zhunhe <xuzhunhe@gmail.com>
2022-02-14 16:20:17 +0900

    feat: Test communication using SIGUSR1, SIGUSR2

--- client.c
+++ client.c
@@ -6,11 +6,12 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 10:19:19 by juhur             #+#    #+#             */
-/*   Updated: 2022/02/14 13:24:52 by juhur            ###   ########.fr       */
+/*   Updated: 2022/02/14 16:18:33 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
+#include <unistd.h>
 
 int	ft_atoi(char *s)
 {
@@ -22,9 +23,28 @@ int	ft_atoi(char *s)
 	return (ret);
 }
 
+void	send(int pid, char c, int count_idx)
+{
+	if (count_idx > 1)
+		send(pid, c / 2, count_idx - 1);
+	usleep(42);
+	if (c & 1)
+		kill(pid, SIGUSR2);
+	else
+		kill(pid, SIGUSR1);
+}
+
 int	main(int argc, char **argv)
 {
+	int	i;
+	int	pid;
+
 	if (argc == 3)
-		kill(ft_atoi(argv[1]), 1);
+	{
+		pid = ft_atoi(argv[1]);
+		i = -1;
+		while (argv[2][++i] != '\0')
+			send(pid, argv[2][i], 8);
+	}
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,10 +6,11 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 10:19:31 by juhur             #+#    #+#             */
-/*   Updated: 2022/02/14 13:21:32 by juhur            ###   ########.fr       */
+/*   Updated: 2022/02/14 16:14:11 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
 #include <unistd.h>
 
 void	print_number(int number, char *charset, int base)
@@ -19,9 +20,22 @@ void	print_number(int number, char *charset, int base)
 	write(1, &charset[number % base], 1);
 }
 
+void	handler(int signo)
+{
+	if (signo == 30)
+		write(1, "0", 1);
+	else
+		write(1, "1", 1);
+}
+
 int	main(void)
 {
+	void	(*hand)(int);
+
 	print_number(getpid(), "0123456789", 10);
+	write(1, "\n", 1);
+	hand = signal(SIGUSR1, handler);
+	hand = signal(SIGUSR2, handler);
 	while (1)
 		;
 	return (0);
