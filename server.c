ce2b7102d8f190b5fe3e3e85cc757b2367d557a1
zhunhe <xuzhunhe@gmail.com>
2022-02-14 18:38:39 +0900

    feat: Print character on server

--- server.c
+++ server.c
@@ -6,34 +6,65 @@
 /*   By: juhur <juhur@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/14 10:19:31 by juhur             #+#    #+#             */
-/*   Updated: 2022/02/14 17:38:18 by juhur            ###   ########.fr       */
+/*   Updated: 2022/02/14 18:25:24 by juhur            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <signal.h>
 #include <unistd.h>
 
-void	print_number(int number, char *charset, int base)
+static char	g_string[9];
+
+static int	ft_strlen(char *s)
+{
+	int	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+static void	print_number(int number, char *charset, int base)
 {
 	if (number / base)
 		print_number(number / base, charset, base);
 	write(1, &charset[number % base], 1);
 }
 
-void	handler(int signo)
+static void	handler(int signo)
 {
+	int	i;
+	int	num;
+
 	if (signo == 30)
-		write(1, "0", 1);
+		g_string[ft_strlen(g_string)] = '0';
 	else
-		write(1, "1", 1);
+		g_string[ft_strlen(g_string)] = '1';
+	if (ft_strlen(g_string) == 8)
+	{
+		i = -1;
+		num = 0;
+		while (++i < 9)
+		{
+			if (i < 8)
+				num = (num << 1) + g_string[i] - '0';
+			g_string[i] = '\0';
+		}
+		write(1, &num, 1);
+	}
 }
 
 int	main(void)
 {
 	void	(*hand)(int);
+	int		i;
 
 	print_number(getpid(), "0123456789", 10);
 	write(1, "\n", 1);
+	i = -1;
+	while (++i < 9)
+		g_string[i] = '\0';
 	hand = signal(SIGUSR1, handler);
 	hand = signal(SIGUSR2, handler);
 	while (1)
